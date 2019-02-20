2b272a8d2ed798ea3b0a083401e2e2a4422cfcd9
Milos Matovic <milos.matovic@odt.net>
2019-02-20 15:39:20 +0100

    handle errors

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,14 +1,31 @@
-const basePort = '9000';
-const baseApi = `http://10.1.200.199:${basePort}`;
+const basePort = '3000';
+const baseApi = `http://localhost:${basePort}`;
 
 export default function Api() {
 
     function login(email) {
-        return fetch(`${baseApi}/login`).then(val => handleResults(val))
+        return fetch(`${baseApi}/login`, postMethod({ email: email }))
+            .then(val => handleResults(val))
+            .catch((err) => handleError(err))
+    }
+
+    function postMethod(payload) {
+        return {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
     }
 
     function handleResults(val) {
         return val.json();
+    }
+
+    function handleError(err) {
+        console.error(`Error: ${err}`);
+        return false;
     }
 
     return {
