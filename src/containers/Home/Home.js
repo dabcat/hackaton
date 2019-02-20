54d3d5d36bff12f6a3c2b597bd8c4bdea4ba0abc
Thomas Fischer <thomas.fischer@oddity.agency>
2019-02-20 15:44:14 +0100

    add Headline and QuestionGroup component to Home component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Headline from '../Headline/Headline';
+import QuestionGroup from '../QuestionGroup/QuestionGroup';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Home component
+                <Headline text="Questions" />
+                <QuestionGroup/>
             </div>
         )
     }
