89941cc8bb945f30b720703ccc19d2e704ba979f
Milos Matovic <milos.matovic@odt.net>
2019-02-20 13:14:27 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
