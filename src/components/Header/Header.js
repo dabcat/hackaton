d2ed7edd8310e41abe810e22649812c27f5a888f
Thomas Fischer <thomas.fischer@oddity.agency>
2019-02-21 11:26:39 +0100

    Add avatar and logo font, update app layout, add QuestionInfoNumbers to Dashboard, improve layouts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { LinkContainer } from "react-router-bootstrap";
 
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Media } from 'react-bootstrap';
+import LogoFont from '../../assets/images/logoFont.png';
+import Avatar from '../../assets/images/avatar.png';
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand>FitnessPal</Navbar.Brand>
+        <Navbar bg="white" expand="lg">
+            <Navbar.Brand><img src={LogoFont} width="120" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <LinkContainer to="/dashboard"><Nav.Link href="#dashboard">Dashboard</Nav.Link></LinkContainer>
-                    <LinkContainer to="/chat"><Nav.Link href="#link">Chat</Nav.Link></LinkContainer>
+                    <LinkContainer to="/chat"><Nav.Link href="#link">Chats</Nav.Link></LinkContainer>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -21,6 +23,18 @@ const Header = () => {
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
+            <Media>
+                <Media.Body>
+                    <h5>Hello Milos!</h5>
+                </Media.Body>
+                <img
+                    width={40}
+                    height={40}
+                    className="mr-3"
+                    src={Avatar}
+                    alt="Generic placeholder"
+                />
+            </Media>
         </Navbar>
     );
 }
--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Headline from '../Headline/Headline';
 import QuestionGroup from '../QuestionGroup/QuestionGroup';
+import './Dashboard.scss';
 
 import Layout from '../../components/Layout/Layout';
 import Header from '../../components/Header/Header';
+import QuestionInfoNumbers from '../QuestionInfoNumbers/QuestionInfoNumbers';
+import { Row } from 'react-bootstrap';
 class Dashboard extends Component {
 
     render() {
         return (
             <Layout>
                 <Header></Header>
-                <div>
-                    <Headline>Recent conversations</Headline>
-                    <QuestionGroup/>
-                </div>
+                <Headline>Recent conversations</Headline>
+                <Row>
+                    <QuestionGroup wrapperClasses="col-lg-9" seeAllLink={true}/>
+                    <QuestionInfoNumbers />
+                </Row>
             </Layout>
         );
     }
